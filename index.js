dcd29228abef635b25df86cf7b7bc7b34c7ad73f
Maksym <maksymsydorovych@gmail.com>
2023-04-12 22:24:17 +0200

    auth

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import {fileURLToPath} from "url";
 import { register } from "./controllers/auth.js";
-
+import authRoutes from "./routes/auth.js";
 //Middleware
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -35,7 +35,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage});
 //AUTH
-app.post("auth/register", upload.single("picture"), register);
+app.post("/auth/register", upload.single("picture"), register);
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
