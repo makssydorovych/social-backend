23cb6952a03e4825a72d1e6ac834b78983436789
Maksym <maksymsydorovych@gmail.com>
2023-04-12 22:00:08 +0200

    User model

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
-import * as path from "path";
+import path from "path";
 import {fileURLToPath} from "url";
+import { register } from "./controllers/auth.js";
 
 //Middleware
 const __filename = fileURLToPath(import.meta.url);
@@ -22,3 +23,24 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+// freeuser free
+//Storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage});
+//AUTH
+app.post("auth/register", upload.single("picture"), register);
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`server port: ${PORT}`));
+}).catch((error) => console.log(`${error} not connected`))
