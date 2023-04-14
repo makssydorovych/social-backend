dda0fd14092e4d83d11f80351a9146b015760861
Maksym <maksymsydorovych@gmail.com>
2023-04-14 17:18:20 +0200

    add some data to DB

--- index.js
+++ index.js
@@ -7,46 +7,58 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import {fileURLToPath} from "url";
-import { register } from "./controllers/auth.js";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
-//Middleware
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
-// freeuser free
-//Storage
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILES */
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "public/assets");
     },
-    filename(req, file, cb) {
+    filename: function (req, file, cb) {
         cb(null, file.originalname);
-    }
+    },
 });
-const upload = multer({storage});
-//AUTH
-app.post("/auth/register", upload.single("picture"), register);
-app.use("/auth", authRoutes);
-//Routes
-app.use("/users", userRoutes);
+const upload = multer({ storage });
 
-const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`server port: ${PORT}`));
-}).catch((error) => console.log(`${error} not connected`))
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE*/
+const PORT = process.env.PORT || 8080;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+
+    })
+    .catch((error) => console.log(`${error} did not connect`));
--- routes/posts.js
+++ routes/posts.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 /* READ */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts", verifyToken, getUserPosts);
+router.get("/:userId/posts", getUserPosts);
 
 /* UPDATE */
-router.patch("/:id/like", verifyToken, likePost);
+router.patch("/:id/like", likePost);
 
 export default router;
