a478346c241c4d650bf07c24360602e6f7ba036c
Maksym <maksymsydorovych@gmail.com>
2023-04-19 14:12:56 +0200

    filepath

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ import User from "./models/User.js";
 import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
