8cde56150450af4fbed2571ddf646febfbee3c5b
Maksym <maksymsydorovych@gmail.com>
2023-04-19 20:27:32 +0200

    fix 2.0

--- src/index.js
+++ src/index.js
@@ -14,9 +14,11 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-
-
+/* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
@@ -27,12 +29,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-	credentials: true,
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-/* FILES */
+/* FILE STORAGE */
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		cb(null, "public/assets");
@@ -52,8 +52,8 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-/* MONGOOSE*/
-const PORT = process.env.PORT || 8080;
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 3001;
 mongoose
 	.connect("mongodb+srv://freeuser:free@cluster0.5jjyyls.mongodb.net/?retryWrites=true&w=majority", {
 		useNewUrlParser: true,
@@ -61,5 +61,9 @@ mongoose
 	})
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+		/* ADD DATA ONE TIME */
+		// User.insertMany(users);
+		// Post.insertMany(posts);
 	})
-	.catch(error => console.log(`${error} did not connect`));
+	.catch((error) => console.log(`${error} did not connect`));
